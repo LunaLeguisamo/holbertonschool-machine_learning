fe8a092e69fa9dca1ea0ceb0491fc630013bfd62
Luna Leguisamo <9356@holbertonstudents.com>
2025-04-03 15:57:25 -0300

    transpose

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
+"""
+Calcula la forma (dimensiones) de una matriz multidimensional.
+"""
 
 def matrix_shape(matrix):
+    """
+    Calcula la forma (dimensiones) de una matriz multidimensional.
+
+    La función usa recursividad para calcular la forma de la matriz.
+    Si la matriz tiene más de una dimensión,
+    la función se llama a sí misma para explorar cada nivel de profundidad
+    y obtener el número de elementos en
+    cada nivel.
+    """
+
     if not isinstance(matrix[0], list):
         return [len(matrix)]
-    
+
     return [len(matrix)] + matrix_shape(matrix[0])
