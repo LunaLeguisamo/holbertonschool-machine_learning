eccf5d9cc94ad7cbf398f8fec5fb13101fe89aed
Luna Leguisamo <9356@holbertonstudents.com>
2025-04-03 15:30:31 -0300

    Shape

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-import numpy as np
-
 
 def matrix_shape(matrix):
-    return list(np.shape(matrix))
+    if not isinstance(matrix[0], list):
+        return [len(matrix)]
+    
+    return [len(matrix)] + matrix_shape(matrix[0])
