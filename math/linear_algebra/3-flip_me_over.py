5ff604a8a20a86a72a99f827fe3acc9a1d67f6a2
Luna Leguisamo <9356@holbertonstudents.com>
2025-04-03 15:57:25 -0300

    transpose

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""
+Calcula la matriz transpuesta de una matriz dada.
+"""
+
+def matrix_transpose(matrix):
+    new_matrix = []
+    for i in range(len(matrix[0])):
+        row = []
+        for j in range(len(matrix)):
+            row.append(matrix[j][i])
+        new_matrix.append(row)
+    return new_matrix
