2b85c2df11f1789bc98315c557a05f762cfe68f7
Luna Leguisamo <9356@holbertonstudents.com>
2025-04-03 16:08:30 -0300

    pycode

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -5,6 +5,11 @@ Calcula la matriz transpuesta de una matriz dada.
 
 
 def matrix_transpose(matrix):
+    """
+    Creamos una nueva matrix y recorremos en simultaneo
+    la matrix original para intercambiar columnas por filas.
+    Por ultimo agregamos la nueva columna a la nueva matriz.
+    """
     new_matrix = []
     for i in range(len(matrix[0])):
         row = []
