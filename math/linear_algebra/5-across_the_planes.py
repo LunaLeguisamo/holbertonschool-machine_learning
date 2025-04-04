a679a33b2dcb5c842cc164a5e2dbd74b7367241b
Luna Leguisamo <9356@holbertonstudents.com>
2025-04-03 22:32:21 -0300

    add matrix 2D

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+"""
+Calcula la forma (dimensiones) de una matriz multidimensional.
+"""
+
+
+def matrix_shape(matrix):
+    """
+    Calcula la forma (dimensiones) de una matriz multidimensional.
+
+    La función usa recursividad para calcular la forma de la matriz.
+    Si la matriz tiene más de una dimensión,
+    la función se llama a sí misma para explorar cada nivel de profundidad
+    y obtener el número de elementos en
+    cada nivel.
+    """
+    if not matrix:
+        return []
+
+    if not isinstance(matrix[0], list):
+        return [len(matrix)]
+
+    return [len(matrix)] + matrix_shape(matrix[0])
+
+
+"""
+Function that add two 2D matrix of the
+the same shape
+"""
+
+
+def add_matrices2D(mat1, mat2):
+    if matrix_shape(mat1) == matrix_shape(mat2):
+        mat3 = []
+        for i in range(len(mat1)):
+            mat4 = []
+            for j in range(len(mat1[0])):
+                mat4.append(mat1[i][j]+mat2[i][j])
+            mat3.append(mat4)
+        return mat3
+    return None
