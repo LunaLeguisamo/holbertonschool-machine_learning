904b35e5b20e71087f9350154391b7e4d7012c9d
Luna Leguisamo <9356@holbertonstudents.com>
2025-04-03 21:57:39 -0300

    pycode add arrays

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,11 +1,31 @@
 #!/usr/bin/env python3
 
 """
+Calcula la forma (dimensiones) de una matriz multidimensional.
+"""
+
+
+def matrix_shape(matrix):
+    """
+    Calcula la forma (dimensiones) de una matriz multidimensional.
+
+    La función usa recursividad para calcular la forma de la matriz.
+    Si la matriz tiene más de una dimensión,
+    la función se llama a sí misma para explorar cada nivel de profundidad
+    y obtener el número de elementos en
+    cada nivel.
+    """
+
+    if not isinstance(matrix[0], list):
+        return [len(matrix)]
+
+    return [len(matrix)] + matrix_shape(matrix[0])
+
+
+"""
 Function that add two arrays of the
 the same shape
 """
-
-import numpy as np
 
 
 def add_arrays(arr1, arr2):
@@ -15,7 +35,7 @@ def add_arrays(arr1, arr2):
     a otra matriz arr3
     """
 
-    if np.shape(arr1) == np.shape(arr2):
+    if matrix_shape(arr1) == matrix_shape(arr2):
         arr3 = []
         for i in range(len(arr1)):
             arr3.append(arr1[i]+arr2[i])
