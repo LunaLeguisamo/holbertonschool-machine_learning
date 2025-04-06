f557ae20b0030ecb97561370637b4ec7bc315ad3
Luna Leguisamo <9356@holbertonstudents.com>
2025-04-05 21:58:34 -0300

    14- matmul

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+This function concatenates two
+numpy arrays along a specified axis
+"""
+
+
+import numpy as np
+
+
+def np_cat(mat1, mat2, axis=0):
+    """
+    This function concatenates
+    two numpy arrays along a specified axis
+    """
+
+    return np.concatenate((mat1, mat2), axis=axis)
