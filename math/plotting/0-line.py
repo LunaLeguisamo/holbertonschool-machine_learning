944cc0c8f5f0b31a8fb4bc0831b65a219d00476d
Luna Leguisamo <9356@holbertonstudents.com>
2025-04-07 17:26:40 -0300

    xlim

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -27,6 +27,6 @@ def line():
     """
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
-    plt.plot(range(11), y, 'r')
     plt.xlim(0, 11)
+    plt.plot(range(11), y, 'r')
     plt.show()
