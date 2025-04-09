cd9553b6b1b65c95dfe6a13c986a4eb934539d64
Luna Leguisamo <9356@holbertonstudents.com>
2025-04-09 20:16:54 -0300

    6-fruits

--- math/plotting/5-all_in_one.py
+++ math/plotting/5-all_in_one.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-
+all in one
 """
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 
 def all_in_one():
     """
+    all in one
     """
     y0 = np.arange(0, 11) ** 3
 
@@ -31,54 +32,48 @@ def all_in_one():
 
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
-    plt.figure(figsize=(7, 6))
 
-    # Primer subplot
-    ax0 = plt.subplot2grid((3, 2), (0, 0))
-    ax0.plot(y0, color='r')
-    ax0.set_xlim(0, 10)
+    fig = plt.figure()
 
-    # Segundo subplot
-    ax1 = plt.subplot2grid((3, 2), (0, 1))
-    ax1.set_title("Men's Height vs Weight", fontsize="x-small")
-    ax1.set_xlabel("Height(in)", fontsize="x-small")
-    ax1.set_ylabel("Weight(lbs)", fontsize="x-small")
-    ax1.scatter(x1, y1, color='purple', s=8)
-    ax1.set_xticks(np.arange(60, 90, 10))
+    fig.suptitle('All in One')
 
-    # Tercer subplot
-    ax2 = plt.subplot2grid((3, 2), (1, 0))
-    ax2.set_title("Exponential Decay of C-14", fontsize="x-small")
-    ax2.set_xlabel("Time (years)", fontsize="x-small")
-    ax2.set_ylabel("Fraction Remaining", fontsize="x-small")
-    ax2.set_xticks(np.arange(0, 28651, 10000))
-    ax2.plot(x2, y2)
-    ax2.set_xlim(0, 28651)
-    ax2.set_yscale('log')
+    fig.add_subplot(321)
+    plt.xlim(0, 10)
+    plt.plot(y0, color='r')
 
-    # Cuarto subplot
-    ax3 = plt.subplot2grid((3, 2), (1, 1))
-    ax3.set_title("Exponential Decay of Radioactive Elements",
-                  fontsize="x-small")
-    ax3.set_xlabel("Time (years)", fontsize="x-small")
-    ax3.set_ylabel("Fraction Remaining", fontsize="x-small")
-    ax3.plot(x3, y31, label='C-14', linestyle='--', color='r')
-    ax3.plot(x3, y32, label='Ra-226', color="green")
-    ax3.set_xlim(0, 20000)
-    ax3.legend(fontsize="x-small")
-    ax3.set_ylim(0, 1)
-    ax3.set_yticks(np.arange(0, 1.1, 0.5))
- 
-    # Quinto subplot
-    ax4 = plt.subplot2grid((3, 2), (2, 0), colspan=2)
-    ax4.set_title("Project A", fontsize="x-small")
-    ax4.set_xlabel("Grades", fontsize="x-small")
-    ax4.set_ylabel("Number of Students", fontsize="x-small")
-    ax4.hist(student_grades, bins=range(0, 110, 10), edgecolor="black")
-    ax4.set_xticks(np.arange(0, 110, 10))
-    ax4.set_xlim(0, 100)
-    ax4.set_ylim(0, 30)
+    fig.add_subplot(322)
+    plt.scatter(x1, y1, color='m')
+    plt.xlabel('Height (in)', size='x-small')
+    plt.ylabel('Weight (lbs)', size='x-small')
+    plt.title("Men's Height vs Weight", size='x-small')
 
-    plt.suptitle("All in One")
+    fig.add_subplot(323)
+    plt.plot(x2, y2)
+    plt.xlabel('Time (years)', size='x-small')
+    plt.ylabel('Fraction Remaining', size='x-small')
+    plt.title("Exponential Decay of C-14", size='x-small')
+    plt.yscale('log')
+    plt.xlim(0, 28650)
+
+    fig.add_subplot(324)
+    plt.plot(x3, y31, 'r--', label="C-14")
+    plt.plot(x3, y32, 'g', label="Ra-226")
+    plt.xlabel("Time (years)", size='x-small')
+    plt.ylabel("Fraction Remaining", size='x-small')
+    plt.title("Exponential Decay of Radioactive Elements", size='x-small')
+    plt.xlim(0, 20000)
+    plt.ylim(0, 1)
+    plt.legend()
+
+    fig.add_subplot(313)
+    bins = range(0, 101, 10)
+    plt.hist(student_grades, bins=bins, edgecolor='black')
+    plt.xlabel('Grades', size='x-small')
+    plt.ylabel("Number of Students", size='x-small')
+    plt.title("Project A", size='x-small')
+    plt.xlim(0, 100)
+    plt.ylim(0, 30)
+    plt.xticks(bins)
+
     plt.tight_layout()
     plt.show()
