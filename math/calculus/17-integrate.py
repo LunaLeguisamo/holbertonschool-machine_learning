e6706f2b0c5ffdbad0ebb891a8f78cddbee925a4
Luna Leguisamo <9356@holbertonstudents.com>
2025-04-12 20:57:46 -0300

    17-integrate

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -4,4 +4,22 @@
 
 
 def poly_integral(poly, C=0):
-    
+    if not isinstance(poly, list) or poly == []:
+        return None
+
+    if not isinstance(C, int):
+        return None
+
+    new_poly = []
+
+    for i in range(len(poly)):
+        if i == 0:
+            new_poly.append(C)
+        elif poly[i] == 0:
+            new_poly.append(0)
+        if poly[i] % (i + 1) == 0:
+            new_value = poly[i] // (i + 1)
+        else:
+            new_value = poly[i] / (i + 1)
+        new_poly.append(new_value)
+    return new_poly
