a6f238d5a6d7e3a71b460f0e3093f8493f7b6eb7
Luna Leguisamo <9356@holbertonstudents.com>
2025-04-26 22:35:54 -0300

    16

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -34,14 +34,12 @@ class DeepNeuralNetwork:
             raise ValueError("nx must be a positive integer")
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
-        if not isinstance(layers, int):
-            raise TypeError("layers must be a list of positive integers")
 
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
 
-        for i in range(1, self.L + 1):
+        for i in range(1, self.L):
             if layers[i] >= 0:
                 if i == 1:
                     prev_nodes = nx
