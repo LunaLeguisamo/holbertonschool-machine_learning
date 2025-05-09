3c12537ebdd788f8184ad2655341cbff910c7971
Luna Leguisamo <9356@holbertonstudents.com>
2025-05-09 17:16:10 -0300

    7-RMSProp

--- supervised_learning/optimization/main.py
+++ supervised_learning/optimization/main.py
@@ -1,64 +1,58 @@
 #!/usr/bin/env python3
 
-import numpy as np
-import tensorflow as tf
 import matplotlib.pyplot as plt
-import random
-import os
+import numpy as np
+update_variables_RMSProp = __import__('7-RMSProp').update_variables_RMSProp
 
-SEED = 0
+def forward_prop(X, W, b):
+    Z = np.matmul(X, W) + b
+    A = 1 / (1 + np.exp(-Z))
+    return A
 
-os.environ['PYTHONHASHSEED'] = str(SEED)
-os.environ['TF_ENABLE_ONEDNN_OPTS']= '0'
-random.seed(SEED)
-np.random.seed(SEED)
-tf.random.set_seed(SEED)
+def calculate_grads(Y, A, W, b):
+    m = Y.shape[0]
+    dZ = A - Y
+    dW = np.matmul(X.T, dZ) / m
+    db = np.sum(dZ, axis=1, keepdims=True) / m
+    return dW, db
 
-create_momentum_op = __import__('6-momentum').create_momentum_op
+def calculate_cost(Y, A):
+    m = Y.shape[0]
+    loss = - (Y * np.log(A) + (1 - Y) * np.log(1 - A))
+    cost = np.sum(loss) / m
 
-def one_hot(Y, classes):
-    """convert an array to a one-hot matrix"""
-    one_hot = np.zeros((Y.shape[0], classes))
-    one_hot[np.arange(Y.shape[0]), Y] = 1
-    return one_hot
+    return cost
 
-lib = np.load('MNIST.npz')
-X_3D = lib['X_train']
-Y = lib['Y_train']
-X = X_3D.reshape((X_3D.shape[0], -1))
-Y_oh=one_hot(Y,10)
+if __name__ == '__main__':
+    lib_train = np.load('Binary_Train.npz')
+    X_3D, Y = lib_train['X'], lib_train['Y'].T
+    X = X_3D.reshape((X_3D.shape[0], -1))
 
-model = tf.keras.models.load_model('data/model.h5', compile=False)
+    nx = X.shape[1]
+    np.random.seed(0)
+    W = np.random.randn(nx, 1)
+    b = 0
+    dW_prev = np.zeros((nx, 1))
+    db_prev = 0
+    for i in range(1000):
+        A = forward_prop(X, W, b)
+        if not (i % 100):
+            cost = calculate_cost(Y, A)
+            print('Cost after {} iterations: {}'.format(i, cost))
+        dW, db = calculate_grads(Y, A, W, b)
+        W, dW_prev = update_variables_RMSProp(0.001, 0.9, 1e-8, W, dW, dW_prev)
+        b, db_prev = update_variables_RMSProp(0.001, 0.9, 1e-8, b, db, db_prev)
+    A = forward_prop(X, W, b)
+    cost = calculate_cost(Y, A)
+    print('Cost after {} iterations: {}'.format(1000, cost))
 
-optimizer=create_momentum_op(0.01, 0.9)
-
-@tf.function
-def train_step(inputs, labels):
-    with tf.GradientTape() as tape:
-        predictions = model(inputs)
-        loss = tf.reduce_mean(tf.keras.losses.CategoricalCrossentropy()(labels, predictions))
-    gradients = tape.gradient(loss, model.trainable_variables)
-    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-    return loss
-
-
-total_iterations = 1000
-for iteration in range(total_iterations):
-
-    cost = train_step(X, Y_oh)
-
-    if (iteration + 1) % 100 == 0:
-        print(f'Cost after {iteration + 1} iterations: {cost}')
-
-
-Y_pred_oh = model(X[:100])
-Y_pred = np.argmax(Y_pred_oh, axis=1)
-
-fig = plt.figure(figsize=(10, 10))
-for i in range(100):
-    fig.add_subplot(10, 10, i + 1)
-    plt.imshow(X_3D[i])
-    plt.title(str(Y_pred[i]))
-    plt.axis('off')
-plt.tight_layout()
-plt.show()
+    Y_pred = np.where(A >= 0.5, 1, 0)
+    fig = plt.figure(figsize=(10, 10))
+    for i in range(100):
+        fig.add_subplot(10, 10, i + 1)
+        plt.imshow(X_3D[i])
+        plt.title(str(Y_pred[i, 0]))
+        plt.axis('off')
+    plt.tight_layout()
+    plt.show()
+    
