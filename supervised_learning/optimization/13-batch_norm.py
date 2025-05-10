413b06c5cee5cbe93990bb254f5438f1ab9f1a3d
Luna Leguisamo <9356@holbertonstudents.com>
2025-05-10 12:10:08 -0300

    13-batch_norm

---
+++ supervised_learning/optimization/13-batch_norm.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+that normalizes an unactivated
+output of a neural network using
+batch normalization
+"""
+
+import numpy as np
+
+
+def batch_norm(Z, gamma, beta, epsilon):
+    """
+    Z is a numpy.ndarray of shape (m, n)
+    that should be normalized
+    m is the number of data points
+    n is the number of features in Z
+    gamma is a numpy.ndarray of shape (1, n)
+    containing the scales used for batch normalization
+    beta is a numpy.ndarray of shape (1, n)
+    containing the offsets used for batch normalization
+    epsilon is a small number used to avoid
+    division by zero
+    Returns: the normalized Z matrix
+    """
+    mean = np.mean(Z, axis=0, keepdims=True)
+    variance = np.var(Z, axis=0, keepdims=True)
+    z_norm = (Z - mean) / np.sqrt(variance + epsilon)
+    z = (gamma * z_norm) + beta
+    return z
--- supervised_learning/optimization/main.py
+++ supervised_learning/optimization/main.py
@@ -1,51 +1,16 @@
 #!/usr/bin/env python3
 
 import numpy as np
-import tensorflow as tf
-import matplotlib.pyplot as plt
-import random
-import os
+batch_norm = __import__('13-batch_norm').batch_norm
 
-SEED = 0
-
-os.environ['PYTHONHASHSEED'] = str(SEED)
-os.environ['TF_ENABLE_ONEDNN_OPTS']= '0'
-random.seed(SEED)
-np.random.seed(SEED)
-tf.random.set_seed(SEED)
-
-learning_rate_decay = __import__('12-learning_rate_decay').learning_rate_decay
-
-def one_hot(Y, classes):
-    """convert an array to a one-hot matrix"""
-    one_hot = np.zeros((Y.shape[0], classes))
-    one_hot[np.arange(Y.shape[0]), Y] = 1
-    return one_hot
-
-lib = np.load('MNIST.npz')
-X_3D = lib['X_train']
-Y = lib['Y_train']
-X = X_3D.reshape((X_3D.shape[0], -1))
-Y_oh=one_hot(Y,10)
-
-model = tf.keras.models.load_model('model.h5', compile=False)
-
-alpha = 0.1
-alpha_schedule =learning_rate_decay(alpha, 1, 10)
-optimizer= tf.keras.optimizers.SGD(learning_rate=alpha_schedule)
-
-@tf.function
-def train_step(inputs, labels):
-    with tf.GradientTape() as tape:
-        predictions = model(inputs)
-        loss = tf.reduce_mean(tf.keras.losses.CategoricalCrossentropy()(labels, predictions))
-    gradients = tape.gradient(loss, model.trainable_variables)
-    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-    return loss
-
-total_iterations = 100
-for iteration in range(total_iterations):
-
-    current_learning_rate = alpha_schedule(iteration).numpy()
-    print(current_learning_rate)
-    cost = train_step(X, Y_oh)
+if __name__ == '__main__':
+    np.random.seed(0)
+    a = np.random.normal(0, 2, size=(100, 1))
+    b = np.random.normal(2, 1, size=(100, 1))
+    c = np.random.normal(-3, 10, size=(100, 1))
+    Z = np.concatenate((a, b, c), axis=1)
+    gamma = np.random.rand(1, 3)
+    beta = np.random.rand(1, 3)
+    print(Z[:10])
+    Z_norm = batch_norm(Z, gamma, beta, 1e-7)
+    print(Z_norm[:10])
