f574e48e3d5be7cf80ac91099dc418af02920d4a
Luna Leguisamo <9356@holbertonstudents.com>
2025-05-18 20:49:39 -0300

    4-drop_f_p

--- supervised_learning/regularization/main.py
+++ supervised_learning/regularization/main.py
@@ -1,43 +1,33 @@
 #!/usr/bin/env python3
 
 import numpy as np
-import tensorflow as tf
-import os
-import random
+dropout_forward_prop = __import__('4-dropout_forward_prop').dropout_forward_prop
 
-SEED = 0
-
-os.environ['PYTHONHASHSEED'] = str(SEED)
-os.environ['TF_ENABLE_ONEDNN_OPTS']= '0'
-random.seed(SEED)
-np.random.seed(SEED)
-tf.random.set_seed(SEED)
-
-l2_reg_cost = __import__('2-l2_reg_cost').l2_reg_cost
-l2_reg_create_layer = __import__('3-l2_reg_create_layer').l2_reg_create_layer
 
 def one_hot(Y, classes):
     """convert an array to a one-hot matrix"""
     m = Y.shape[0]
-    one_hot = np.zeros((m, classes))
-    one_hot[np.arange(m), Y] = 1
+    one_hot = np.zeros((classes, m))
+    one_hot[Y, np.arange(m)] = 1
     return one_hot
 
-lib= np.load('MNIST.npz')
-X_train_3D = lib['X_train']
-Y_train = lib['Y_train']
-X_train = X_train_3D.reshape((X_train_3D.shape[0], -1))
-Y_train_oh = one_hot(Y_train, 10)
+if __name__ == '__main__':
+    lib= np.load('MNIST.npz')
+    X_train_3D = lib['X_train']
+    Y_train = lib['Y_train']
+    X_train = X_train_3D.reshape((X_train_3D.shape[0], -1)).T
+    Y_train_oh = one_hot(Y_train, 10)
 
-input_shape = X_train.shape[1]
+    np.random.seed(0)
 
-x = tf.keras.Input(shape=input_shape)
-h1 = l2_reg_create_layer(x, 256, tf.nn.tanh, 0.05)  
-y_pred = l2_reg_create_layer(h1, 10, tf.nn.softmax, 0.)   
-model = tf.keras.Model(inputs=x, outputs=y_pred)
+    weights = {}
+    weights['W1'] = np.random.randn(256, 784)
+    weights['b1'] = np.zeros((256, 1))
+    weights['W2'] = np.random.randn(128, 256)
+    weights['b2'] = np.zeros((128, 1))
+    weights['W3'] = np.random.randn(10, 128)
+    weights['b3'] = np.zeros((10, 1))
 
-Predictions = model(X_train)
-cost = tf.keras.losses.CategoricalCrossentropy()(Y_train_oh, Predictions)
-
-l2_cost = l2_reg_cost(cost,model)
-print(l2_cost)
+    cache = dropout_forward_prop(X_train, weights, 3, 0.8)
+    for k, v in sorted(cache.items()):
+        print(k, v)
