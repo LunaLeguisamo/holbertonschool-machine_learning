bee157b1c0f6a81d15f5a736873c7acd88fe7f8c
Luna Leguisamo <9356@holbertonstudents.com>
2025-05-20 19:30:24 -0300

    0-convolve

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -1,34 +1,36 @@
 #!/usr/bin/env python3
-
 """
-
+Function that performs a valid convolution on grayscale images:
 """
 import numpy as np
 
 
 def convolve_grayscale_valid(images, kernel):
     """
-    images is a numpy.ndarray with shape (m, h, w)
-    containing multiple grayscale images
-    m is the number of images
-    h is the height in pixels of the images
-    w is the width in pixels of the images
-    kernel is a numpy.ndarray with shape (kh, kw)
-    containing the kernel for the convolution
-    kh is the height of the kernel
-    kw is the width of the kernel
+    Performs a valid convolution on grayscale images.
+
+    Parameters:
+    - images: numpy.ndarray of shape (m, h, w)
+    - kernel: numpy.ndarray of shape (kh, kw)
+
+    Returns:
+    - numpy.ndarray containing the convolved images
     """
     m, h, w = images.shape
-
     kh, kw = kernel.shape
+
     new_h = h - kh + 1
     new_w = w - kw + 1
 
     output = np.zeros((m, new_h, new_w))
 
-    for i in range(kw):
-        for j in range(kh):
-            slice = images[:, i:i+kw, j:j+kh]
+    for i in range(new_h):
+        for j in range(new_w):
+            # Extraer la porción de cada imagen
+            # que se corresponde con el kernel
+            slice = images[:, i:i+kh, j:j+kw]
+            # Realizar la multiplicación elemento a
+            # elemento y sumar para obtener un escalar por imagen
             output[:, i, j] = np.sum(slice * kernel, axis=(1, 2))
 
     return output
