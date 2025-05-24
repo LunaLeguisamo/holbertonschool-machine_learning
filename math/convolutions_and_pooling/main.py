7fcad6e9ffe3a00834a7a066f9f8d4840e084434
Luna Leguisamo <9356@holbertonstudents.com>
2025-05-24 18:21:38 -0300

    4-channels

--- math/convolutions_and_pooling/main.py
+++ math/convolutions_and_pooling/main.py
@@ -2,21 +2,22 @@
 import os
 import matplotlib.pyplot as plt
 import numpy as np
-convolve_grayscale = __import__('3-convolve_grayscale').convolve_grayscale
+convolve_channels = __import__('4-convolve_channels').convolve_channels
 
 
 if __name__ == '__main__':
 
-    dataset = np.load('MNIST.npz')
-    images = dataset['X_train']
+    dataset = np.load('animals_1.npz')
+    images = dataset['data']
     print(images.shape)
-    kernel = np.array([[1 ,0, -1], [1, 0, -1], [1, 0, -1]])
-    images_conv = convolve_grayscale(images, kernel, padding='valid', stride=(2, 2))
+    kernel = np.array([[[0, 0, 0], [-1, -1, -1], [0, 0, 0]], [[-1, -1, -1], [5, 5, 5], [-1, -1, -1]], [[0, 0, 0], [-1, -1, -1], [0, 0, 0]]])
+    images_conv = convolve_channels(images, kernel, padding='valid')
     print(images_conv.shape)
-    os.makedirs('images_3', exist_ok=True)
-    plt.imshow(images[0], cmap='gray')
-    plt.savefig('images_3/01.png')
-    plt.show()
-    plt.imshow(images_conv[0], cmap='gray')
+
+    plt.imshow(images[0])
     plt.savefig('images_3/02.png')
     plt.show()
+    os.makedirs('images_3', exist_ok=True)
+    plt.imshow(images_conv[0])
+    plt.show()
+    
