cbc9377dc4ff1513b1e7408f0e53e10a3621306c
Luna Leguisamo <9356@holbertonstudents.com>
2025-05-29 11:11:20 -0300

    1-poool

---
+++ supervised_learning/cnn/1-pool_forward.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""
+Function that performs forward propagation
+over a pooling layer of a neural network
+"""
+
+import numpy as np
+
+
+def pool_forward(A_prev, kernel_shape, stride=(1, 1), mode='max'):
+    """
+    A_prev is a numpy.ndarray of shape (m, h_prev, w_prev, c_prev)
+    containing the output of the previous layer
+    m is the number of examples
+    h_prev is the height of the previous layer
+    w_prev is the width of the previous layer
+    c_prev is the number of channels in the previous layer
+    kernel_shape is a tuple of (kh, kw) containing the size of
+    the kernel for the pooling
+    kh is the kernel height
+    kw is the kernel width
+    stride is a tuple of (sh, sw) containing the strides for the pooling
+    sh is the stride for the height
+    sw is the stride for the width
+    mode is a string containing either max or avg, indicating whether to
+    perform maximum or average pooling, respectively
+    you may import numpy as np
+    Returns: the output of the pooling layer
+    """
+    m, h_prev, w_prev, c_prev = A_prev.shape
+    kh, kw = kernel_shape
+    sh, sw = stride
+
+    h_output = ((h_prev - kh) // sh) + 1
+    w_output = ((w_prev - kw) // sw) + 1
+
+    output = np.zeros((m, h_output, w_output, c_prev))
+
+    for i in range(m):
+        for j in range(h_output):
+            for k in range(w_output):
+                for c in range(c_prev):
+                    start_h = j * sh
+                    end_h = start_h + kh
+                    start_w = k * sw
+                    end_w = start_w + kw
+                    slicing = A_prev[i, start_h:end_h, start_w:end_w, :]
+                    if mode == 'max':
+                        output[i, j, k, c] = np.max(slicing)
+                    if mode == 'avg':
+                        output[i, j, k, c] = np.mean(slicing)
+
+    return output
--- supervised_learning/cnn/main.py
+++ supervised_learning/cnn/main.py
@@ -2,24 +2,23 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-conv_forward = __import__('0-conv_forward').conv_forward
+pool_forward = __import__('1-pool_forward').pool_forward
 
 if __name__ == "__main__":
     np.random.seed(0)
     lib = np.load('MNIST.npz')
     X_train = lib['X_train']
     m, h, w = X_train.shape
-    X_train_c = X_train.reshape((-1, h, w, 1))
+    X_train_a = X_train.reshape((-1, h, w, 1))
+    X_train_b = 1 - X_train_a
+    X_train_c = np.concatenate((X_train_a, X_train_b), axis=3)
 
-    W = np.random.randn(3, 3, 1, 2)
-    b = np.random.randn(1, 1, 1, 2)
-
-    def relu(Z):
-        return np.maximum(Z, 0)
-
-    plt.imshow(X_train[0])
+    print(X_train_c.shape)
+    plt.imshow(X_train_c[0, :, :, 0])
     plt.show()
-    A = conv_forward(X_train_c, W, b, relu, padding='valid')
+    plt.imshow(X_train_c[0, :, :, 1])
+    plt.show()
+    A = pool_forward(X_train_c, (2, 2), stride=(2, 2))
     print(A.shape)
     plt.imshow(A[0, :, :, 0])
     plt.show()
