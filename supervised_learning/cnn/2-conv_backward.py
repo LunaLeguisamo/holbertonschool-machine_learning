7f9c12a2c1ff3acba87db126d3c624bee25701ec
Luna Leguisamo <9356@holbertonstudents.com>
2025-05-31 15:23:25 -0300

    2a

--- supervised_learning/cnn/2-conv_backward.py
+++ supervised_learning/cnn/2-conv_backward.py
@@ -36,50 +36,51 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     the kernels (dW), and the biases (db), respectively
     """
     
-    m, h_new, w_new, c_new = dZ.shape
     m, h_prev, w_prev, c_prev = A_prev.shape
-    kh, kw, _, _ = W.shape
+    kh, kw, _, c_new = W.shape
     sh, sw = stride
+    m, h_new, w_new, _ = dZ.shape
 
-    # Calcular padding si es necesario
-    if padding == 'valid':
-        ph, pw = 0, 0
-    elif padding == 'same':
+    # Initialize derivatives
+    dA_prev = np.zeros_like(A_prev)
+    dW = np.zeros_like(W)
+    db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
+
+    # Compute padding
+    if padding == "same":
         ph = ((h_prev - 1) * sh + kh - h_prev) // 2 + 1
         pw = ((w_prev - 1) * sw + kw - w_prev) // 2 + 1
     else:
-        ph, pw = padding
-        
-    # Padding para A_prev y su gradiente
-    A_prev_pad = np.pad(A_prev,
-                        pad_width=((0, 0), (ph, ph), (pw, pw), (0, 0)),
-                        mode='constant')
-    #dA_prev_pad = np.pad(dA_prev,
-    #                     pad_width=((0, 0), (ph, ph), (pw, pw), (0, 0)),
-    #\                     mode='constant')
-    
-    # Inicializar gradientes
-    dA_prev_pad = np.zeros_like(A_prev_pad)
-    dW = np.zeros_like(W)
-    db = db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
-    
-    # Loop para cada ejemplo del batch
-    for h in range(h_new):
-        for w in range(w_new):
-            for c in range(c_new):
-                # Encontrar corners de la ventana
-                vert_start = h * sh
-                vert_end = vert_start + kh
-                horiz_start = w * sw
-                horiz_end = horiz_start + kw
+        ph = 0
+        pw = 0
 
-                # Calcular gradientes
-                dA_prev_pad[:, vert_start:vert_end, horiz_start:horiz_end, :] += (W[:, :, :, c] * dZ[:, h, w, c, np.newaxis, np.newaxis, np.newaxis])
-                dW[:, :, :, c] += np.sum(A_prev_pad[:, vert_start:vert_end, horiz_start:horiz_end, :] * 
-                                            dZ[:, h, w, c, np.newaxis, np.newaxis, np.newaxis], axis=0)
+    # Pad A_prev and dA_prev
+    A_prev_pad = np.pad(A_prev, ((0,), (ph,), (pw,), (0,)), mode='constant')
+    dA_prev_pad = np.pad(dA_prev, ((0,), (ph,), (pw,), (0,)), mode='constant')
 
-    # Recortar padding
-    if padding == 'same':
-        dA_prev_pad = dA_prev_pad[:, ph:-ph, pw:-pw, :]
+    for i in range(m):  # loop over the batch of training examples
+        a_prev_pad = A_prev_pad[i]
+        da_prev_pad = dA_prev_pad[i]
 
-    return dA_prev_pad, dW, db
+        for h in range(h_new):
+            for w in range(w_new):
+                for c in range(c_new):
+                    vert_start = h * sh
+                    vert_end = vert_start + kh
+                    horiz_start = w * sw
+                    horiz_end = horiz_start + kw
+
+                    # Slice the region
+                    a_slice = a_prev_pad[vert_start:vert_end, horiz_start:horiz_end, :]
+
+                    # Update gradients
+                    da_prev_pad[vert_start:vert_end, horiz_start:horiz_end, :] += W[:, :, :, c] * dZ[i, h, w, c]
+                    dW[:, :, :, c] += a_slice * dZ[i, h, w, c]
+
+        # Save the updated dA_prev for the i-th example
+        if padding == "same":
+            dA_prev[i, :, :, :] = da_prev_pad[ph:-ph or None, pw:-pw or None, :]
+        else:
+            dA_prev[i, :, :, :] = da_prev_pad
+
+    return dA_prev, dW, db
