c163c5ffbf0ab3987f097545fb15eb78559bb841
Luna Leguisamo <9356@holbertonstudents.com>
2025-06-08 20:34:21 -0300

    ffgd

--- supervised_learning/deep_cnns/5-dense_block.py
+++ supervised_learning/deep_cnns/5-dense_block.py
@@ -8,50 +8,45 @@ from tensorflow import keras as K
 
 def dense_block(X, nb_filters, growth_rate, layers):
     """
-    Builds a dense block as described in DenseNet-B.
-
-    Parameters:
-    - X: input tensor
-    - nb_filters: number of filters in the input tensor
-    - growth_rate: growth rate (number of filters to add per layer)
-    - layers: number of layers in the dense block
-
-    Returns:
-    - concatenated output of the dense block
-    - updated number of filters
-    """
-    he_init = K.initializers.he_normal(seed=0)
-    concat = X
+    Builds a dense block as described in Densely Connected Convolutional Networks
     
-    for i in range(layers):
-        # BatchNorm + ReLU
-        bn = K.layers.BatchNormalization()(concat)
-        act = K.layers.ReLU()(bn)
+    Args:
+        X: output from the previous layer
+        nb_filters: integer representing the number of filters in X
+        growth_rate: growth rate for the dense block
+        layers: number of layers in the dense block
         
-        # 1x1 bottleneck convolution
-        bottleneck = K.layers.Conv2D(
-            filters=4 * growth_rate,
-            kernel_size=1,
-            strides=1,
-            padding='same',
-            kernel_initializer=he_init,
-            use_bias=False)(act)
-
-        # BatchNorm + ReLU
-        bn2 = K.layers.BatchNormalization()(bottleneck)
-        act2 = K.layers.ReLU()(bn2)
+    Returns:
+        The concatenated output of each layer within the Dense Block
+        The number of filters within the concatenated outputs
+    """
+    for i in range(layers):
+        # Batch Normalization followed by ReLU activation
+        X1 = K.layers.BatchNormalization()(X)
+        X1 = K.layers.Activation('relu')(X1)
+        
+        # Bottleneck layer: 1x1 conv to reduce feature maps (DenseNet-B)
+        X1 = K.layers.Conv2D(filters=4 * growth_rate,
+                            kernel_size=(1, 1),
+                            strides=(1, 1),
+                            padding='same',
+                            kernel_initializer=K.initializers.he_normal(seed=0))(X1)
+        
+        # Batch Normalization followed by ReLU activation
+        X1 = K.layers.BatchNormalization()(X1)
+        X1 = K.layers.Activation('relu')(X1)
         
         # 3x3 convolution
-        conv = K.layers.Conv2D(
-            filters=growth_rate,
-            kernel_size=3,
-            strides=1,
-            padding='same',
-            kernel_initializer=he_init,
-            use_bias=False)(act2)
-
-        # Concatenate all previous features with output of this layer
-        concat = K.layers.Concatenate()([concat, conv])
+        X1 = K.layers.Conv2D(filters=growth_rate,
+                            kernel_size=(3, 3),
+                            strides=(1, 1),
+                            padding='same',
+                            kernel_initializer=K.initializers.he_normal(seed=0))(X1)
+        
+        # Concatenate the input with the new feature maps
+        X = K.layers.concatenate([X, X1])
+        
+        # Update the number of filters
         nb_filters += growth_rate
-
-    return concat, nb_filters
+    
+    return X, nb_filters
