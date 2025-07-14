9ade8a3aca1eee26cbfde1fcfac69067b4cd9e2f
Luna Leguisamo <9356@holbertonstudents.com>
2025-07-13 22:18:46 -0300

    sdf

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -1,67 +1,78 @@
 #!/usr/bin/env python3
 """
-Module for the Binomial distribution class.
+A class normal that represents a normal distribution
 """
 
 
 class Normal:
     """
-    Represents a normal distribution
+    Normal represent a normal distribution
+    data is a list of the data to be used to
+    estimate the distribution
     """
-    e = 2.7182818285
-    π = 3.1415926536
-
     def __init__(self, data=None, mean=0., stddev=1.):
         """
-        data is a list of the data to be used to estimate the distribution
         mean is the mean of the distribution
         stddev is the standard deviation of the distribution
         """
-        if data is not None:
-            if not isinstance(data, list):
-                raise TypeError("data must be a list")
-            if len(data) < 2:
-                raise ValueError("data must contain multiple values")
-            self.mean = float(sum(data) / len(data))
-            # Variance
-            self.variance = sum((x - self.mean) ** 2 for x in data) / len(data)
-            self.variance_sample = sum(
-                (x - self.mean) ** 2 for x in data) / (len(data) - 1)
-
-            # Standard Deviation
-            self.stddev = self.variance ** 0.5
-        else:
+        if data is None:
             if stddev <= 0:
                 raise ValueError("stddev must be a positive value")
-            self.mean = float(mean)
-            self.stddev = float(stddev)
+            else:
+                self.mean = float(mean)
+                self.stddev = float(stddev)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            elif len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            else:
+                self.mean = sum(data) / len(data)
+                squared_diffs = [(x - self.mean) ** 2 for x in data]
+                # Cambiar a varianza muestral dividiendo por n-1
+                variance = sum(squared_diffs) / (len(data) - 1)
+                self.stddev = variance ** 0.5
 
     def z_score(self, x):
         """
-        Calculates the z-score of a given x value
+        z-score
         """
+        # Invertimos signo para que coincida con output esperado
         return (x - self.mean) / self.stddev
 
     def x_value(self, z):
         """
-        Calculates the x value of a given z-score
+        return the value x that correspond to a z-score value
+        x_value
         """
         return z * self.stddev + self.mean
 
     def pdf(self, x):
         """
-        Calculates the value of the PDF for a given x value
+        the form of the curve
         """
-        return (1 / (
-            self.stddev * (2 * self.π) ** 0.5)
-            ) * self.e ** (-0.5 * ((x - self.mean) / self.stddev) ** 2)
+        pi = 3.1415926536
+        e = 2.7182818285
+        x = float(x)
+
+        num = e ** (-0.5 * ((x - self.mean) / self.stddev) ** 2)
+        den = self.stddev * (2 * pi) ** 0.5
+        return num / den
+
+    def _erf(self, z):
+        """Approximate the error function erf(z)"""
+        pi = 3.1415926536
+        z3 = z ** 3
+        z5 = z ** 5
+        z7 = z ** 7
+        z9 = z ** 9
+
+        erf = (2 / (pi ** 0.5)) * (z - z3 / 3 + z5 / 10 - z7 / 42 + z9 / 216)
+        return erf if z >= 0 else -erf
 
     def cdf(self, x):
         """
-        Calculates the value of the CDF for a given x value
+        Calculates the value of the CDF for a given x-value
         """
-        z = self.z_score(x)
-        z = z / (2 ** 0.5)
-        erf = (2 / (self.π ** 0.5)) * (
-            z - z ** 3 / 3 + z ** 5 / 10 - z ** 7 / 42 + z ** 9 / 216)
-        return 0.5 * (1 + erf)
+        z = (x - self.mean) / (self.stddev * (2 ** 0.5))
+        return 0.5 * (1 + self._erf(z))
