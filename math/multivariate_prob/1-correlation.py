6636f11fc7464436ca0364d736cdfae0ac889a0c
Luna Leguisamo <9356@holbertonstudents.com>
2025-07-14 15:02:26 -0300

    2-correlation

---
+++ math/multivariate_prob/1-correlation.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Function  that calculates a correlation matrix
+"""
+import numpy as np
+
+
+def correlation(C):
+    """
+    C is a numpy.ndarray of shape (d, d) containing a covariance matrix
+    d is the number of dimensions
+    If C is not a numpy.ndarray, raise a TypeError with the message C must
+    be a numpy.ndarray
+    If C does not have shape (d, d), raise a ValueError with the message C
+    must be a 2D square matrix
+    Returns a numpy.ndarray of shape (d, d) containing the correlation
+    matrix
+    """
+    if not isinstance(C, np.ndarray):
+        raise TypeError("C must be a numpy.ndarray")
+
+    if C.shape[0] != C.shape[1]:
+        raise ValueError("C must be a 2D square matrix")
+
+    variances = np.diag(C)
+    stddev = np.sqrt(variances)
+    denom = np.outer(stddev, stddev)
+    correlation = C / denom
+    np.fill_diagonal(correlation, 1.0)
+
+    return correlation
--- math/multivariate_prob/main.py
+++ math/multivariate_prob/main.py
@@ -2,10 +2,10 @@
 
 if __name__ == '__main__':
     import numpy as np
-    mean_cov = __import__('0-mean_cov').mean_cov
+    correlation = __import__('1-correlation').correlation
 
-    np.random.seed(0)
-    X = np.random.multivariate_normal([12, 30, 10], [[36, -30, 15], [-30, 100, -20], [15, -20, 25]], 10000)
-    mean, cov = mean_cov(X)
-    print(mean)
-    print(cov)
+    C = np.array([[36, -30, 15], [-30, 100, -20], [15, -20, 25]])
+    Co = correlation(C)
+    print(C)
+    print(Co)
+    
