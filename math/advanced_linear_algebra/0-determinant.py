e03bcbb1128bad6207032d7ee1dc968f84db1140
Luna Leguisamo <9356@holbertonstudents.com>
2025-07-17 18:10:21 -0300

    0-det

---
+++ math/advanced_linear_algebra/0-determinant.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+Function that calculates the determinant of a matrix
+"""
+
+
+def determinant(matrix):
+    """
+    matrix is a list of lists whose determinant should be calculated
+    If matrix is not a list of lists, raise a TypeError with the message
+    matrix must be a list of lists
+    If matrix is not square, raise a ValueError with the message matrix
+    must be a square matrix
+    The list [[]] represents a 0x0 matrix
+    Returns: the determinant of matrix
+    """
+    if matrix == []:
+        raise TypeError("matrix must be a list of lists")
+
+    if matrix == [[]]:  # 0x0 matrix
+        return 1
+
+    if not all(len(row) == len(matrix) for row in matrix):
+        raise ValueError("matrix must be a square matrix")
+
+    size = len(matrix)
+
+    if size == 1:
+        return matrix[0][0]
+
+    if size == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+    det = 0
+    for i, elem in enumerate(matrix[0]):
+        minor = [row[:i] + row[i+1:] for row in matrix[1:]]
+        det += ((-1) ** i) * elem * determinant(minor)
+
+    return det
