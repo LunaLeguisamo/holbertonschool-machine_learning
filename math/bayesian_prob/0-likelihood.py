b81c790b4de3159dc16a255f7d56fe5ba6cbed3d
Luna Leguisamo <9356@holbertonstudents.com>
2025-07-23 14:41:13 -0300

    fghfg

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -17,7 +17,7 @@ def likelihood(x, n, P):
     obtaining the data, x and n, for each probability in P, respectively
     """
 
-    if n <= 0 or not isinstance(n, int):
+    if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
 
     if not isinstance(x, int) or x < 0:
