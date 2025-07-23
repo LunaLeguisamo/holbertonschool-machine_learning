209a60aed65cbfd080fb4809e25e92d300134480
Luna Leguisamo <9356@holbertonstudents.com>
2025-07-23 13:49:19 -0300

    likelihood

---
+++ math/bayesian_prob/0-likelihood.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+Function that calculates the likelihood
+of obtaining this data given various hypothetical
+probabilities of developing severe side effects
+"""
+import numpy as np
+import scipy as sp
+
+
+def likelihood(x, n, P):
+    """
+    x is the number of patients that develop severe side effects
+    n is the total number of patients observed
+    P is a 1D numpy.ndarray containing the various hypothetical
+    probabilities of developing severe side effects
+    Returns: a 1D numpy.ndarray containing the likelihood of
+    obtaining the data, x and n, for each probability in P, respectively
+    """
+
+    if n <= 0 or not isinstance(n, int):
+        raise ValueError("n must be a positive integer")
+
+    if not isinstance(x, int) or x <= 0:
+        raise ValueError("x must be an integer\
+            that is greater than or equal to 0")
+
+    if not isinstance(P, np.ndarray):
+        raise TypeError("P must be a 1D numpy.ndarray")
+
+    if np.any((P < 0) | (P > 1)):
+        raise ValueError("All values in P must be in the range [0, 1]")
+
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+
+    binomio = sp.special.comb(n, x)
+    likeh_arr = []
+    for p in P:
+        lk = binomio * (p ** x) * ((1 - p) ** (n-x))
+        likeh_arr.append(lk)
+        array = np.array(likeh_arr)
+
+    return array
