fecbdf2cefce6d9d637716c6852026f7d2bfd7f7
Luna Leguisamo <9356@holbertonstudents.com>
2025-07-23 16:38:47 -0300

    pppp

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -73,13 +73,11 @@ def intersection(x, n, P, Pr):
 
     if np.any((P < 0) | (P > 1)):
         raise ValueError(
-            "All values in P must be in the range [0, 1] where P\
-                is the incorrect variable")
+            "All values in P must be in the range [0, 1]")
 
     if np.any((Pr < 0) | (Pr > 1)):
         raise ValueError(
-            "All values in Pr must be in the range [0, 1] where Pr\
-                is the incorrect variable")
+            "All values in Pr must be in the range [0, 1]")
 
     if not np.isclose(np.sum(Pr), 1):
         raise ValueError("Pr must sum to 1")
