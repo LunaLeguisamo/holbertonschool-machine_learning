48659e7ece3146bdcbca37b677bb74b7b71cefce
Luna Leguisamo <9356@holbertonstudents.com>
2025-07-31 14:35:05 -0300

    variance

---
+++ unsupervised_learning/clustering/2-variance.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Function that calculates the total intra-cluster variance for a data set
+"""
+import numpy as np
+
+
+def variance(X, C):
+    """
+    X is a numpy.ndarray of shape (n, d) containing the data set
+    C is a numpy.ndarray of shape (k, d) containing the centroid means
+    for each cluster
+    You are not allowed to use any loops
+    Returns: var, or None on failure
+    var is the total variance
+    """
+    diff = X[:, np.newaxis, :] - C[np.newaxis, :, :]
+    distances = np.linalg.norm(diff, axis=2)
+    dis_min = np.min(distances, axis=1)
+    var = np.sum(dis_min ** 2)
+    return var
