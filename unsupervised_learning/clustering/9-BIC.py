2b8d13a69e7ac67324e32b83132b02739f232a87
Luna Leguisamo <9356@holbertonstudents.com>
2025-08-03 19:12:53 -0300

    hjhubhv

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -17,41 +17,36 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
 
     n, d = X.shape
 
-    if not isinstance(kmin, int) or kmin < 1 or kmin >= n:
+    if not isinstance(kmin, int) or kmin < 1 or kmin > n:
         return None, None, None, None
 
     if kmax is None:
         kmax = n
 
-    if not isinstance(kmax, int) or kmax < kmin or kmax >= n:
+    if not isinstance(kmax, int) or kmax < kmin or kmax > n:
         return None, None, None, None
 
     best_k = None
     best_result = None
+    best_bic = None
     log_likelihoods = []
     bics = []
 
     for k in range(kmin, kmax + 1):
         results = expectation_maximization(X, k, iterations, tol, verbose)
-
         if results is None:
             return None, None, None, None
-
         pi, m, S, g, log_likelihood = results
 
-        # Guardamos el log_likelihood
         log_likelihoods.append(log_likelihood)
 
-        # Cantidad de parámetros
         p = (k * d * (d + 1)) / 2 + d * k + (k - 1)
-
-        # Cálculo del BIC
         bic = p * np.log(n) - 2 * log_likelihood
         bics.append(bic)
 
-        # Guardamos el mejor resultado
-        if best_result is None or bic < bics[best_k - kmin]:
+        if best_bic is None or bic < best_bic:
+            best_bic = bic
             best_k = k
             best_result = (pi, m, S)
 
-    return best_k, best_result, log_likelihoods, bics
+    return best_k, best_result, np.array(log_likelihoods), np.array(bics)
