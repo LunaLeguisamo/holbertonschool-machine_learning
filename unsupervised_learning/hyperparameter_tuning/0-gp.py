7bcaed0843c790b57e34d411617096ea9f96a34e
Luna Leguisamo <9356@holbertonstudents.com>
2025-08-16 21:22:48 -0300

    0-gp

---
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+Class hat represents a noiseless 1D Gaussian process
+"""
+import numpy as np
+
+
+class GaussianProcess:
+    """
+    X_init is a numpy.ndarray of shape (t, 1) representing the
+    inputs already sampled with the black-box function
+    Y_init is a numpy.ndarray of shape (t, 1) representing the
+    outputs of the black-box function for each input in X_init
+    t is the number of initial samples
+    l is the length parameter for the kernel
+    sigma_f is the standard deviation given to the output of the
+    black-box function
+    Sets the public instance attributes X, Y, l, and sigma_f
+    corresponding to the respective constructor inputs
+    Sets the public instance attribute K, representing the current
+    covariance kernel matrix for the Gaussian process
+    """
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        """
+        Class constructor
+        X_init: numpy.ndarray (t, 1) inputs already sampled
+        Y_init: numpy.ndarray (t, 1) outputs of the black-box function
+        l: length parameter
+        sigma_f: standard deviation of output
+        """
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+
+        # Covariance matrix of the initial points
+        self.K = self.kernel(self.X, self.X)
+
+    def kernel(self, X1, X2):
+        """
+        Computes the covariance kernel matrix using RBF
+        X1: numpy.ndarray (m, 1)
+        X2: numpy.ndarray (n, 1)
+        Returns: covariance matrix (m, n)
+        """
+        # Expand dimensions to compute pairwise squared distances
+        sqdist = np.sum(X1**2, 1).reshape(-1, 1) + \
+            np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
+
+        return (self.sigma_f ** 2) * np.exp(-0.5 / self.l**2 * sqdist)
