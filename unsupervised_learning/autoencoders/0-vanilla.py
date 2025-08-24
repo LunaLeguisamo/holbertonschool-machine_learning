af0d16fd331d94611e4b27c1735d4ba3b5e7867a
Luna Leguisamo <9356@holbertonstudents.com>
2025-08-24 16:51:25 -0300

    0-vanilla

---
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""
+Function that creates an autoencoder
+"""
+import tensorflow.keras as keras
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims):
+    """
+    input_dims is an integer containing the dimensions of the model input
+    hidden_layers is a list containing the number of nodes for each hidden
+    ayer in the encoder, respectively the hidden layers should be reversed
+    for the decoder
+    latent_dims is an integer containing the dimensions of the latent
+    space representation
+    Returns: encoder, decoder, auto
+    encoder is the encoder model
+    decoder is the decoder model
+    auto is the full autoencoder model
+    The autoencoder model should be compiled using adam optimization and
+    binary cross-entropy loss
+    All layers should use a relu activation except for the last layer in
+    the decoder, which should use sigmoid
+    """
+
+    # Encoder
+    encoder_input = keras.Input(shape=(input_dims,))
+    x = encoder_input
+    for units in hidden_layers:
+        x = keras.layers.Dense(units, activation="relu")(x)
+    latent = keras.layers.Dense(latent_dims, activation="relu")(x)
+    encoder = keras.Model(inputs=encoder_input, outputs=latent)
+
+    # Decoder
+    decoder_input = keras.Input(shape=(latent_dims,))
+    y = decoder_input
+    for units in reversed(hidden_layers):
+        y = keras.layers.Dense(units, activation="relu")(y)
+    decoder_output = keras.layers.Dense(input_dims, activation="sigmoid")(y)
+    decoder = keras.Model(inputs=decoder_input, outputs=decoder_output)
+
+    # Autoencoder
+    auto_output = decoder(encoder(encoder_input))
+    auto = keras.Model(inputs=encoder_input, outputs=auto_output)
+
+    # Compile with adam optimizer + binary crossentropy loss
+    auto.compile(optimizer="adam", loss="binary_crossentropy")
+
+    return encoder, decoder, auto
