c31eefbd66ad29dc4415c7dc4ff924a66b424269
Luna Leguisamo <9356@holbertonstudents.com>
2025-08-31 18:17:57 -0300

    0-simplegan

--- unsupervised_learning/autoencoders/2-convolutional.py
+++ unsupervised_learning/autoencoders/2-convolutional.py
@@ -12,65 +12,42 @@ def autoencoder(input_dims, filters, latent_dims):
     latent_dims: tuple -> dimensions of the latent space representation
     Returns: encoder, decoder, auto
     """
-    
-    # Build encoder
-    encoder_inputs = keras.layers.Input(shape=input_dims)
-    x = encoder_inputs
-    
-    # Track dimensions through the encoder
-    current_height, current_width = input_dims[0], input_dims[1]
-    
-    # Add convolutional layers to encoder
+    # ----- ENCODER -----
+    encoder_input = keras.Input(shape=input_dims)
+    z = encoder_input
+
     for f in filters:
-        x = keras.layers.Conv2D(f, (3, 3), activation='relu', padding='same')(x)
-        x = keras.layers.MaxPooling2D((2, 2), padding='same')(x)
-        # Update dimensions after pooling (for same padding, dimensions are halved, rounded up)
-        current_height = (current_height + 1) // 2
-        current_width = (current_width + 1) // 2
-    
-    # Final convolution for latent space
-    latent = keras.layers.Conv2D(latent_dims[2], (3, 3), activation='relu', padding='same')(x)
-    
-    # Create encoder model
-    encoder = keras.models.Model(encoder_inputs, latent, name='encoder')
-    
-    # Build decoder
-    decoder_inputs = keras.layers.Input(shape=latent_dims)
-    x = decoder_inputs
-    
-    # Calculate how many upsampling steps we need
-    # We need to get from latent_dims back to input_dims
-    target_height, target_width = input_dims[0], input_dims[1]
-    
-    # Add convolutional layers with upsampling
-    reversed_filters = list(reversed(filters))
-    
-    for i, f in enumerate(reversed_filters):
-        x = keras.layers.Conv2D(f, (3, 3), activation='relu', padding='same')(x)
-        
-        # Only upsample if we're not at the target dimensions yet
-        current_decoder_height = x.shape[1] if x.shape[1] is not None else latent_dims[0]
-        current_decoder_width = x.shape[2] if x.shape[2] is not None else latent_dims[1]
-        
-        if current_decoder_height * 2 <= target_height and current_decoder_width * 2 <= target_width:
-            x = keras.layers.UpSampling2D((2, 2))(x)
-    
-    # If we're still not at the target dimensions, add more upsampling
-    while x.shape[1] is not None and x.shape[1] < target_height and x.shape[2] is not None and x.shape[2] < target_width:
-        x = keras.layers.UpSampling2D((2, 2))(x)
-    
-    # Final convolution to reconstruct original image
-    decoder_outputs = keras.layers.Conv2D(input_dims[-1], (3, 3), 
-                                         activation='sigmoid', padding='same')(x)
-    
-    # Create decoder model
-    decoder = keras.models.Model(decoder_inputs, decoder_outputs, name='decoder')
-    
-    # Build autoencoder
-    autoencoder_outputs = decoder(encoder(encoder_inputs))
-    auto = keras.models.Model(encoder_inputs, autoencoder_outputs, name='autoencoder')
-    
-    # Compile autoencoder
-    auto.compile(optimizer='adam', loss='binary_crossentropy')
-    
+        z = keras.layers.Conv2D(f, (3, 3), activation="relu", padding="same")(z)
+        z = keras.layers.MaxPooling2D((2, 2), padding="same")(z)
+
+    encoder_output = z
+    encoder = keras.Model(encoder_input, encoder_output, name="encoder")
+
+    # ----- DECODER -----
+    decoder_input = keras.Input(shape=latent_dims)
+    z = decoder_input
+
+    # Apply reversed filters except the first one
+    for f in filters[::-1][1:]:
+        z = keras.layers.Conv2D(f, (3, 3), activation="relu", padding="same")(z)
+        z = keras.layers.UpSampling2D((2, 2))(z)
+
+    # Penultimate convolution with valid padding
+    z = keras.layers.Conv2D(filters[0], (3, 3), activation="relu", padding="same")(z)
+
+    # Final reconstruction layer
+    decoder_output = keras.layers.Conv2D(
+        input_dims[-1], (3, 3), activation="sigmoid", padding="same"
+    )(z)
+
+    decoder = keras.Model(decoder_input, decoder_output, name="decoder")
+
+    # ----- AUTOENCODER -----
+    auto_input = keras.Input(shape=input_dims)
+    encoded = encoder(auto_input)
+    decoded = decoder(encoded)
+    auto = keras.Model(auto_input, decoded, name="autoencoder")
+
+    auto.compile(optimizer="adam", loss="binary_crossentropy")
+
     return encoder, decoder, auto
---
+++ unsupervised_learning/gan/0-simple_gan.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+"""
+0-simple_gan.py
+"""
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+
+
+class Simple_GAN(keras.Model):
+    """Simple GAN class"""
+    def __init__(self, generator, discriminator, latent_generator,
+                 real_examples, batch_size=200, disc_iter=2,
+                 learning_rate=0.005):
+        super().__init__()
+        self.latent_generator = latent_generator
+        self.real_examples = real_examples
+        self.generator = generator
+        self.discriminator = discriminator
+        self.batch_size = batch_size
+        self.disc_iter = disc_iter
+
+        self.learning_rate = learning_rate
+        self.beta_1 = 0.5
+        self.beta_2 = 0.9
+
+        # define the generator loss and optimizer:
+        self.generator.loss =\
+            lambda x: tf.keras.losses.MeanSquaredError()(x, tf.ones(x.shape))
+        self.generator.optimizer = keras.optimizers.Adam(
+            learning_rate=self.learning_rate,
+            beta_1=self.beta_1,
+            beta_2=self.beta_2
+        )
+        self.generator.compile(
+            optimizer=self.generator.optimizer,
+            loss=self.generator.loss
+            )
+
+        # define the discriminator loss and optimizer:
+        self.discriminator.loss = lambda x, y: \
+            tf.keras.losses.MeanSquaredError()(x, tf.ones(x.shape))\
+            + tf.keras.losses.MeanSquaredError()(y, -1 * tf.ones(y.shape))
+        self.discriminator.optimizer = keras.optimizers.Adam(
+            learning_rate=self.learning_rate,
+            beta_1=self.beta_1,
+            beta_2=self.beta_2
+        )
+        self.discriminator.compile(
+            optimizer=self.discriminator.optimizer,
+            loss=self.discriminator.loss
+            )
+
+    def get_fake_sample(self, size=None, training=False):
+        if not size:
+            size = self.batch_size
+        return self.generator(self.latent_generator(size),
+                              training=training
+                              )
+
+    def get_real_sample(self, size=None):
+        if not size:
+            size = self.batch_size
+        sorted_indices = tf.range(tf.shape(self.real_examples)[0])
+        random_indices = tf.random.shuffle(sorted_indices)[:size]
+        return tf.gather(self.real_examples, random_indices)
+
+    def train_step(self, useless_argument):
+        # Train discriminator disc_iter times
+        for _ in range(self.disc_iter):
+            with tf.GradientTape() as tape:
+                real_sample = self.get_real_sample()
+                fake_sample = self.get_fake_sample(training=True)
+                real_pred = self.discriminator(real_sample, training=True)
+                fake_pred = self.discriminator(fake_sample, training=True)
+                discr_loss = tf.keras.losses.MeanSquaredError()(
+                    real_pred, tf.ones_like(real_pred)) +\
+                    tf.keras.losses.MeanSquaredError()(
+                    fake_pred, -1 * tf.ones_like(fake_pred)
+                            )
+            grads = tape.gradient(
+                discr_loss,
+                self.discriminator.trainable_variables
+                )
+            self.discriminator.optimizer.apply_gradients(
+                zip(grads, self.discriminator.trainable_variables)
+            )
+
+        # Train generator once
+        with tf.GradientTape() as tape:
+            fake_sample = self.get_fake_sample(training=True)
+            fake_pred = self.discriminator(fake_sample, training=True)
+            gen_loss = tf.keras.losses.MeanSquaredError()(
+                fake_pred,
+                tf.ones_like(fake_pred))
+        grads = tape.gradient(gen_loss, self.generator.trainable_variables)
+        self.generator.optimizer.apply_gradients(
+            zip(grads, self.generator.trainable_variables)
+        )
+
+        return {"discr_loss": discr_loss, "gen_loss": gen_loss}
