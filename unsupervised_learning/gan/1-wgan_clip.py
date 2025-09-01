92cc1c5951d4c1c00ab0f5fea8dc1e7d2398a731
Luna Leguisamo <9356@holbertonstudents.com>
2025-08-31 21:09:22 -0300

    1-wgan

--- unsupervised_learning/gan/1-wgan_clip.py
+++ unsupervised_learning/gan/1-wgan_clip.py
@@ -9,7 +9,9 @@ import matplotlib.pyplot as plt
 
 
 class WGAN_clip(keras.Model):
-    def __init__(self, generator, discriminator, latent_generator, real_examples, batch_size=200, disc_iter=2, learning_rate=.005):
+    def __init__(self, generator, discriminator, latent_generator,
+                 real_examples, batch_size=200, disc_iter=2,
+                 learning_rate=.005):
         super().__init__()
         self.latent_generator = latent_generator
         self.real_examples = real_examples
@@ -25,21 +27,31 @@ class WGAN_clip(keras.Model):
         # define the generator loss and optimizer:
         self.generator.loss = lambda x: -tf.reduce_mean(x)
         self.generator.optimizer = keras.optimizers.Adam(
-            learning_rate=self.learning_rate, beta_1=self.beta_1, beta_2=self.beta_2
+            learning_rate=self.learning_rate,
+            beta_1=self.beta_1,
+            beta_2=self.beta_2
         )
-        self.generator.compile(optimizer=self.generator.optimizer, loss=self.generator.loss)
+        self.generator.compile(
+            optimizer=self.generator.optimizer,
+            loss=self.generator.loss)
 
         # define the discriminator loss and optimizer:
-        self.discriminator.loss = lambda x, y: tf.reduce_mean(y) - tf.reduce_mean(x)
+        self.discriminator.loss =\
+            lambda x, y: tf.reduce_mean(y) - tf.reduce_mean(x)
         self.discriminator.optimizer = keras.optimizers.Adam(
-            learning_rate=self.learning_rate, beta_1=self.beta_1, beta_2=self.beta_2
+            learning_rate=self.learning_rate,
+            beta_1=self.beta_1,
+            beta_2=self.beta_2
         )
-        self.discriminator.compile(optimizer=self.discriminator.optimizer, loss=self.discriminator.loss)
+        self.discriminator.compile(
+            optimizer=self.discriminator.optimizer,
+            loss=self.discriminator.loss)
 
     def get_fake_sample(self, size=None, training=False):
         if not size:
             size = self.batch_size
-        return self.generator(self.latent_generator(size), training=training)
+        return self.generator(self.latent_generator(size),
+                              training=training)
 
     def get_real_sample(self, size=None):
         if not size:
@@ -49,36 +61,35 @@ class WGAN_clip(keras.Model):
         return tf.gather(self.real_examples, random_indices)
 
     def train_step(self, useless_argument):
+        """Train the WGAN model"""
         for _ in range(self.disc_iter):
             with tf.GradientTape() as tape:
                 real_sample = self.get_real_sample()
                 fake_sample = self.get_fake_sample(training=True)
-                
                 real_pred = self.discriminator(real_sample, training=True)
                 fake_pred = self.discriminator(fake_sample, training=True)
-                
-                discr_loss = self.discriminator.loss(fake_pred, real_pred)
-                
-            grads = tape.gradient(discr_loss, self.discriminator.trainable_variables)
-            
+
+                discr_loss = self.discriminator.loss(real_pred, fake_pred)
+
+            grads = tape.gradient(discr_loss,
+                                  self.discriminator.trainable_variables
+                                  )
+
             self.discriminator.optimizer.apply_gradients(
                 zip(grads, self.discriminator.trainable_variables)
             )
-            
+
             for var in self.discriminator.trainable_variables:
                 var.assign(tf.clip_by_value(var, -1.0, 1.0))
 
         with tf.GradientTape() as tape:
             fake_sample = self.get_fake_sample(training=True)
             fake_pred = self.discriminator(fake_sample, training=True)
-            
             gen_loss = self.generator.loss(fake_pred)
-            
+
         grads = tape.gradient(gen_loss, self.generator.trainable_variables)
-        
         self.generator.optimizer.apply_gradients(
             zip(grads, self.generator.trainable_variables)
         )
 
         return {"discr_loss": discr_loss, "gen_loss": gen_loss}
-    
