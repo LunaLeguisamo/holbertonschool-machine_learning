2e095d9bdb04252bed98bddf8167dcf5c79a1c1f
Luna Leguisamo <9356@holbertonstudents.com>
2025-10-03 19:11:49 -0300

    0-dataset trained

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -1,51 +1,34 @@
 #!/usr/bin/env python3
 """
-Module: 0-dataset.py
-Creates and preprocesses dataset for machine translation
+0-dataset.py
 """
-
 import tensorflow_datasets as tfds
 import transformers
 
 
 class Dataset:
-    """Loads and preps a dataset for machine translation"""
-
     def __init__(self):
-        """Initialize dataset and tokenizers"""
-        # Load the dataset with both data and info
-        dataset, info = tfds.load('ted_hrlr_translate/pt_to_en',
-                                  with_info=True,
-                                  as_supervised=True,
-                                  shuffle_files=False))
+        dataset, _ = tfds.load('ted_hrlr_translate/pt_to_en',
+                             as_supervised=True,
+                             with_info=True)
 
-        # Create instance attributes
         self.data_train = dataset['train']
         self.data_valid = dataset['validation']
-
-        # Create tokenizers using the training data
-        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
-            self.data_train
-            )
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(self.data_train)
 
     def tokenize_dataset(self, data):
-        """
-        Creates sub-word tokenizers for the dataset
+        # Extraer oraciones
+        pt_texts, en_texts = [], []
+        for pt, en in data:
+            pt_texts.append(pt.numpy().decode('utf-8'))
+            en_texts.append(en.numpy().decode('utf-8'))
 
-        Args:
-            data: tf.data.Dataset with (portuguese, english) tuples
+        # Cargar y entrenar tokenizers
+        tokenizer_pt = transformers.AutoTokenizer.from_pretrained("neuralmind/bert-base-portuguese-cased")
+        tokenizer_en = transformers.AutoTokenizer.from_pretrained("bert-base-uncased")
 
-        Returns:
-            tokenizer_pt: Portuguese tokenizer
-            tokenizer_en: English tokenizer
-        """
-        # Load pre-trained tokenizers
-        # Portuguese tokenizer
-        tokenizer_pt = transformers.AutoTokenizer.from_pretrained(
-            "neuralmind/bert-base-portuguese-cased")
-
-        # English tokenizer
-        tokenizer_en = transformers.AutoTokenizer.from_pretrained(
-            "bert-base-uncased")
+        # Entrenar con tamaño de vocabulario 8192
+        tokenizer_pt = tokenizer_pt.train_new_from_iterator(pt_texts, vocab_size=2**13)
+        tokenizer_en = tokenizer_en.train_new_from_iterator(en_texts, vocab_size=2**13)
 
         return tokenizer_pt, tokenizer_en
