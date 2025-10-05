9a66c34b25cfb828f959b8df97a1dccc9a5a0b6d
Luna Leguisamo <9356@holbertonstudents.com>
2025-10-05 16:57:10 -0300

    3-dataset

--- supervised_learning/transformer_apps/main.py
+++ supervised_learning/transformer_apps/main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
-Dataset = __import__('2-dataset').Dataset
+Dataset = __import__('3-dataset').Dataset
+import tensorflow as tf
 
-data = Dataset()
+tf.random.set_seed(0)
+data = Dataset(32, 40)
 for pt, en in data.data_train.take(1):
     print(pt, en)
 for pt, en in data.data_valid.take(1):
