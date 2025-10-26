aa285667d33d4f1935dc4d2238d6d374e989c959
Luna Leguisamo <9356@holbertonstudents.com>
2025-10-26 18:03:19 -0300

    epsilon-greedy

--- reinforcement_learning/q_learning/main.py
+++ reinforcement_learning/q_learning/main.py
@@ -2,17 +2,14 @@
 
 load_frozen_lake = __import__('0-load_env').load_frozen_lake
 q_init = __import__('1-q_init').q_init
+epsilon_greedy = __import__('2-epsilon_greedy').epsilon_greedy
+import numpy as np
 
-env = load_frozen_lake()
-Q = q_init(env)
-print(Q.shape)
-env = load_frozen_lake(is_slippery=True)
-Q = q_init(env)
-print(Q.shape)
 desc = [['S', 'F', 'F'], ['F', 'H', 'H'], ['F', 'F', 'G']]
 env = load_frozen_lake(desc=desc)
 Q = q_init(env)
-print(Q.shape)
-env = load_frozen_lake(map_name='4x4')
-Q = q_init(env)
-print(Q.shape)
+Q[7] = np.array([0.5, 0.7, 1, -1])
+np.random.seed(0)
+print(epsilon_greedy(Q, 7, 0.5))
+np.random.seed(1)
+print(epsilon_greedy(Q, 7, 0.5))
