191690d22e830f46df31fe5f3f3f419a87ad6936
Luna Leguisamo <9356@holbertonstudents.com>
2025-10-26 16:43:56 -0300

    0-env_lake

---
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Load and initialize the environment
+for Q-learning.
+"""
+import gymnasium as gym
+
+
+def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
+    """Load the Frozen Lake environment from OpenAI Gym.
+
+    Args:
+        desc (list of list of str, optional): Custom
+        description of the map.
+        map_name (str, optional): Predefined map name.
+        is_slippery (bool): Whether the ice is slippery.
+
+    Returns:
+        gym.Env: The initialized Frozen Lake environment.
+    """
+    env = gym.make(
+        'FrozenLake-v1', desc=desc, map_name=map_name,
+        is_slippery=is_slippery
+        )
+    return env
