eba1b836ccc48b90fc8e934f12eae55acee5e240
Luna Leguisamo <9356@holbertonstudents.com>
2025-10-27 19:50:45 -0300

    q_learning

--- reinforcement_learning/q_learning/main.py
+++ reinforcement_learning/q_learning/main.py
@@ -2,14 +2,16 @@
 
 load_frozen_lake = __import__('0-load_env').load_frozen_lake
 q_init = __import__('1-q_init').q_init
-epsilon_greedy = __import__('2-epsilon_greedy').epsilon_greedy
+train = __import__('3-q_learning').train
 import numpy as np
 
+np.random.seed(0)
 desc = [['S', 'F', 'F'], ['F', 'H', 'H'], ['F', 'F', 'G']]
 env = load_frozen_lake(desc=desc)
 Q = q_init(env)
-Q[7] = np.array([0.5, 0.7, 1, -1])
-np.random.seed(0)
-print(epsilon_greedy(Q, 7, 0.5))
-np.random.seed(1)
-print(epsilon_greedy(Q, 7, 0.5))
+
+Q, total_rewards  = train(env, Q)
+print(Q)
+split_rewards = np.split(np.array(total_rewards), 10)
+for i, rewards in enumerate(split_rewards):
+    print((i+1) * 500, ':', np.mean(rewards))
