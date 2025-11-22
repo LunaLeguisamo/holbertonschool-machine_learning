a18806c40a580e3c417968ac46c6eccec62586ce
Luna Leguisamo <9356@holbertonstudents.com>
2025-11-22 19:22:48 -0300

    1-frm_dic

--- pipeline/pandas/1-from_dictionary.py
+++ pipeline/pandas/1-from_dictionary.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python3
 import pandas as pd
 """
-Write a python script that creates a pd.DataFrame from a dictionary:
+Write a python script that creates a pd.DataFrame from a dictionary
+"""
+
+"""
 The first column should be labeled First and have the values
 0.0, 0.5, 1.0, and 1.5
 The second column shoud be labeled Second and have the values
 one, two, three, four
-The rows should be labeled A, B, C, and D, respectively
-The pd.DataFrame should be saved into the variable df
-
 """
-
 data = {
     "First": [0.0, 0.5, 1.0, 1.5],
     "Second": ["one", "two", "three", "four"]
 }
 
+"""
+The rows should be labeled A, B, C, and D, respectively
+"""
 index = ["A", "B", "C", "D"]
+
+"""
+The pd.DataFrame should be saved into the variable df
+"""
 df = pd.DataFrame(data, index=index)
