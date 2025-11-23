59b29ea80c025d52f56780c96c88a60dd48daabc
Luna Leguisamo <9356@holbertonstudents.com>
2025-11-23 16:12:10 -0300

    9-fill

--- pipeline/pandas/main.py
+++ pipeline/pandas/main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
 from_file = __import__('2-from_file').from_file
-prune = __import__('8-prune').prune
+fill = __import__('9-fill').fill
 
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
-df = prune(df)
+df = fill(df)
 
 print(df.head())
+print(df.tail())
