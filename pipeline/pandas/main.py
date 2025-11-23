85e87f4427f1e50b2687e4f9909c0da87389cf32
Luna Leguisamo <9356@holbertonstudents.com>
2025-11-23 16:23:32 -0300

    11-cooncat

--- pipeline/pandas/main.py
+++ pipeline/pandas/main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
 from_file = __import__('2-from_file').from_file
-index = __import__('10-index').index
+concat = __import__('11-concat').concat
 
-df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+df1 = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+df2 = from_file('bitstampUSD_1-min_data_2012-01-01_to_2020-04-22.csv', ',')
 
-df = index(df)
+df = concat(df1, df2)
 
-print(df.tail())
+print(df)
