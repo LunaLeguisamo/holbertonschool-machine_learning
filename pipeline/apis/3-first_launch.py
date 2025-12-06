e914d8e923627dc6e9bb9f229d91c7efe2573e0c
Luna Leguisamo <9356@holbertonstudents.com>
2025-12-06 11:29:45 -0300

    3-firsstlaunch

--- pipeline/apis/3-first_launch.py
+++ pipeline/apis/3-first_launch.py
@@ -6,19 +6,22 @@ import requests
 from datetime import datetime
 import sys
 
+
 def get_first_launch():
+    """Fetches and returns information about
+    the first SpaceX launch."""
     # Fetch all launches
     launches_resp = requests.get("https://api.spacexdata.com/v4/launches")
     launches = launches_resp.json()
-    
+
     # Sort by date_unix ascending
     launches = sorted(
-        [l for l in launches if l.get("date_unix")],
+        [le for le in launches if le.get("date_unix")],
         key=lambda x: x["date_unix"]
     )
 
     first = launches[0]  # first launch
-    
+
     # Extract fields
     name = first.get("name")
     date_unix = first.get("date_unix")
@@ -29,16 +32,22 @@ def get_first_launch():
     date_local = datetime.fromtimestamp(date_unix).astimezone().isoformat()
 
     # Get rocket name
-    rocket_resp = requests.get(f"https://api.spacexdata.com/v4/rockets/{rocket_id}")
-    rocket_name = rocket_resp.json().get("name")
+    rocket_resp =\
+        requests.get(f"https://api.spacexdata.com/v4/rockets/{rocket_id}")
+    rocket_name =\
+        rocket_resp.json().get("name")
 
     # Get launchpad info
-    launchpad_resp = requests.get(f"https://api.spacexdata.com/v4/launchpads/{launchpad_id}")
+    launchpad_resp =\
+        requests.get(
+            f"https://api.spacexdata.com/v4/launchpads/{launchpad_id}"
+            )
     launchpad_data = launchpad_resp.json()
     launchpad_name = launchpad_data.get("name")
     launchpad_locality = launchpad_data.get("locality")
 
-    return f"{name} ({date_local}) {rocket_name} - {launchpad_name} ({launchpad_locality})"
+    return f"{name} ({date_local}) {rocket_name}\
+        - {launchpad_name} ({launchpad_locality})"
 
 
 if __name__ == "__main__":
