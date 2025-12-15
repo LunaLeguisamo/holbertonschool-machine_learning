1ffef92bad632bc85c144f64fb12bdb38034656e
Luna Leguisamo <9356@holbertonstudents.com>
2025-12-14 23:12:36 -0300

    5

--- pipeline/data_augmentation/5-hue.py
+++ pipeline/data_augmentation/5-hue.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 """
-Write a function that changes the hue of an image
-image is a 3D tf.Tensor containing the image to change
-delta is the amount the image hue should be changed
-Returns the altered image
+Function that changes the hue of an image
 """
 
 import tensorflow as tf
@@ -11,21 +8,9 @@ import tensorflow as tf
 
 def change_hue(image, delta):
     """
-    Changes the hue of an RGB image.
-
-    Args:
-        image: 3D tf.Tensor of shape (H, W, 3), dtype float32 or uint8.
-               If uint8, it will be converted to float32 in [0,1].
-        delta: Amount to change the hue, float in [-1, 1].
-
-    Returns:
-        The hue-adjusted image as a tf.Tensor, same shape as input.
+    image is a 3D tf.Tensor containing the image to change
+    delta is the amount the hue should change
+    Returns the altered image
     """
-    # tf.image.adjust_hue expects floats in [0, 1]
-    if image.dtype == tf.uint8:
-        image = tf.image.convert_image_dtype(image, tf.float32)
-
-    # Apply hue adjustment
-    image = tf.image.adjust_hue(image, delta)
-
-    return image
+    image_hue = tf.image.adjust_hue(image, delta)
+    return image_hue
